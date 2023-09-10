a0c71f9e92c91cc6f3b6f79a623cb6733bf521e8
Alex <alexastrum@gmail.com>
2023-09-10 01:13:38 -0700

    Deploy nft contract to testnet

--- contract/counter/testnet-deploy.sh
+++ contract/counter/testnet-deploy.sh
@@ -13,18 +13,17 @@ seid=~/go/bin/seid
 
 # $seid keys show $keyname
 admin_addr=$(printf $password | $seid keys show $keyname | grep -A 1 "address" | sed -n 's/.*address: //p')
-# printf "%s addr: %s\n" $keyname $admin_addr
+printf "%s addr: %s\n" $keyname $admin_addr
+$seid q bank balances $admin_addr --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2
 
-# $seid q bank balances $admin_addr --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2
-
-$seid tx wasm store $contract -y --from=$keyname --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2 --gas=1100000 --fees=1100000usei --broadcast-mode=block
-
+# $seid tx wasm store $contract -y --from=$keyname --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2 --gas=1100000 --fees=1100000usei --broadcast-mode=block
+code=2814
 # code=$(printf $password | $seid tx wasm store $contract -y --from=$keyname --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2 --gas=30000000 --fees=1000000usei --broadcast-mode=block | grep -A 1 "code_id" | sed -n 's/.*value: "//p' | sed -n 's/"//p')
-# printf "Code id: %s\n" $code
-
 # $seid q bank balances $admin_addr --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2
+printf "Code id: %s\n" $code
 
+# $seid tx wasm instantiate $code '{"count": 0}' --from $keyname --broadcast-mode=block --label "counter-aarch64" --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2 --gas=200000 --fees=200000usei --admin=$admin_addr -y
+addr=sei17h76djf3xymwtstccmr8lz9l4p3e5nj5nezwzylya6fuww4dt3fse853ak
 # addr=$(printf $password | $seid tx wasm instantiate $code '{"count": 0}' --from $keyname --broadcast-mode=block --label "counter-aarch64" --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2 --gas=30000000 --fees=1000000usei --admin=$admin_addr -y | grep -A 1 -m 1 "key: _contract_address" | sed -n 's/.*value: //p' | xargs)
-# printf "Deployed contract address: %s\n" $addr
-
+printf "Deployed contract address: %s\n" $addr
 # $seid q bank balances $admin_addr --node https://rpc.atlantic-2.seinetwork.io --chain-id atlantic-2
---
+++ nft/install.sh
@@ -0,0 +1,20 @@
+# Install lighthouse globally
+npm install -g @we-bump/lighthouse-cli
+
+# Run the app
+lighthouse init
+
+ # Clone this repository 
+git clone https://github.com/We-Bump/Lighthouse-mint-ui
+
+# Go into the repository
+cd Lighthouse-mint-ui
+
+# Install dependencies
+npm install
+
+# To build
+npm run build
+
+# To start
+npm start
